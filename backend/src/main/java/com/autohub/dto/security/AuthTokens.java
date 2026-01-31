6bc164299a31c9c3985394bcac7711b372ea2b7b
WDVinuka <wdvwwickramaarachchi@students.nsbm.ac.lk>
2026-01-31 11:20:42 +0530

    Created AuthTokens.java file.

---
+++ backend/src/main/java/com/autohub/dto/security/AuthTokens.java
@@ -0,0 +1,15 @@
+package com.autohub.dto.security;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AuthTokens {
+    @NotNull
+    private final String accessToken;
+
+    @NotNull
+    private final String refreshToken;
+}
